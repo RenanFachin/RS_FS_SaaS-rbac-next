3ac1285afe89183b10f4eeef3803518831201f5f
Renan <renan.fachin@hotmail.com>
2024-04-25 15:41:32 -0300

    chore: eslint initial settings

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,34 +1,10 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
-/** @type {import("eslint").Linter.Config} */
+/** @type{import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
+  extends: [
+    '@rocketseat/eslint-config/react'
   ],
-  overrides: [
-    {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
-};
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error'
+  }
+}
--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,10 +1,8 @@
 /** @type{import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    '@rocketseat/eslint-config/next'
-  ],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort'],
   rules: {
-    'simple-import-sort/imports': 'error'
-  }
+    'simple-import-sort/imports': 'error',
+  },
 }

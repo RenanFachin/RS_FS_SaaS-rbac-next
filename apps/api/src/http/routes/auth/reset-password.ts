aab4131a3c7940f22fcabf38cea8740aeb6dd1e2
Renan <renan.fachin@hotmail.com>
2024-05-01 23:56:05 -0300

    fix: Enable cascades and delete tokens in the password reset route

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -40,15 +40,22 @@ export async function resetPassword(app: FastifyInstance) {
       // criptografar a nova senha
       const passwordHash = await hash(password, 6)
 
-      // Atualizar a senha do usuário
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+      // Atualizar a senha do usuário e deleta o token existente
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send()
     },

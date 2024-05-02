df2291c0a2841614ae1a1ae4dce967e87dcbc367
Renan <renan.fachin@hotmail.com>
2024-05-01 22:55:40 -0300

    feat: Creates route for creating invitations from organizations to members

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,7 +18,9 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invites/create-invite'
 import { getOrganizationMembers } from './routes/members/get-organization-members'
+import { removeMember } from './routes/members/remove-member'
 import { updateMemberRole } from './routes/members/update-member-role'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
@@ -32,7 +34,6 @@ import { deleteProject } from './routes/projects/delete-project'
 import { getProject } from './routes/projects/get-project'
 import { getProjects } from './routes/projects/get-projects'
 import { updateProject } from './routes/projects/update-project'
-import { removeMember } from './routes/members/remove-member'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -96,6 +97,8 @@ app.register(updateProject)
 app.register(getOrganizationMembers)
 app.register(updateMemberRole)
 app.register(removeMember)
+
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Http Server Running!')

c0cff49885bda242e512dbcc38e0b33cba392c12
Renan <renan.fachin@hotmail.com>
2024-05-01 23:20:51 -0300

    feat: Creates routes to list all invitations sent by an organization and user action routes to invitations (accept and reject)

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,8 +18,10 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInviteDetails } from './routes/invites/get-invite-details'
+import { getInvites } from './routes/invites/get-invites'
 import { getOrganizationMembers } from './routes/members/get-organization-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMemberRole } from './routes/members/update-member-role'
@@ -35,6 +37,7 @@ import { deleteProject } from './routes/projects/delete-project'
 import { getProject } from './routes/projects/get-project'
 import { getProjects } from './routes/projects/get-projects'
 import { updateProject } from './routes/projects/update-project'
+import { rejectInvite } from './routes/invites/reject-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -101,6 +104,9 @@ app.register(removeMember)
 
 app.register(createInvite)
 app.register(getInviteDetails)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Http Server Running!')
